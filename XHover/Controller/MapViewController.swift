0d637828f50170fea31447f097ac4c54b8b567fd
Luis <luisgnzlz33@gmail.com>
2021-09-22 09:05:27 -0700

    added a map to the map VC

--- XHover/Controller/MapViewController.swift
+++ XHover/Controller/MapViewController.swift
@@ -6,13 +6,34 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    let mapView = MKMapView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .white
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let leftMargin:CGFloat = 10
+        let topMargin:CGFloat = 60
+        let mapWidth:CGFloat = view.frame.size.width-20
+        let mapHeight:CGFloat = 300
+        
+        mapView.frame = CGRect(x: leftMargin, y: topMargin, width: mapWidth, height: mapHeight)
+        
+        mapView.mapType = MKMapType.standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        
+        // Or, if needed, we can position map in the center of the view
+        mapView.center = view.center
+        
+        view.addSubview(mapView)
     }
     
 }
---
+++ XHover/View/FPVMapView.swift
@@ -0,0 +1,14 @@
+//
+//  FPVMapView.swift
+//  XHover
+//
+//  Created by Luis Gonzalez on 9/20/21.
+//
+
+import UIKit
+import MapKit
+
+class FPVMapView: MKMapView {
+
+    
+}
